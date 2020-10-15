5d4df9252c12dd68c19741dcee357816cd28e10f
William Dam <damw@oregonstate.edu>
2020-10-15 13:12:33 -0700

    Added user profile storyboard with linked view controller.

---
+++ Soapbox/UserProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UserProfileViewController.swift
+//  Soapbox
+//
+//  Created by william dam on 10/15/20.
+//
+
+import UIKit
+
+class UserProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
