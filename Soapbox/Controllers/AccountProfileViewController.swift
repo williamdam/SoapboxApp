7d6f66b078be6716f9ce7c4d940827d0aacab61d
William Dam <damw@oregonstate.edu>
2020-10-16 12:07:59 -0700

    Resolve file structure.

---
+++ Soapbox/Controllers/AccountProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AccountProfileViewController.swift
+//  Soapbox
+//
+//  Created by william dam on 10/16/20.
+//
+
+import UIKit
+
+class AccountProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
