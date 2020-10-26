9436e090a49a9c015c5a3d2c185ce242ddd7afce
William Dam <damw@oregonstate.edu>
2020-10-26 11:57:02 -0700

    Sign up page.

--- SoapboxNew/View/SignupViewController.swift
+++ SoapboxNew/View/SignupViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class SignupViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,4 +36,10 @@ class SignupViewController: UIViewController {
     }
     */
 
+    @IBAction func signUpButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+    
 }
