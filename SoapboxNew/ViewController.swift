70865c1d3c2f7f5cb1b2e5c1bd0bc79a3024fced
William Dam <damw@oregonstate.edu>
2020-10-26 10:18:13 -0700

    Initial Commit

---
+++ SoapboxNew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoapboxNew
+//
+//  Created by william dam on 10/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
