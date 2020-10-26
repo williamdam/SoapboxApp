77ace7f0c0f3e1aec85b40e99dd1251de4bd9b6d
William Dam <damw@oregonstate.edu>
2020-10-26 10:34:07 -0700

    View controllers setup

---
+++ SoapboxNew/View/SignupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignupViewController.swift
+//  SoapboxNew
+//
+//  Created by william dam on 10/26/20.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
